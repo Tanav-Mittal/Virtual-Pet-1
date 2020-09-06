3675f841398d678f6cf3979d78b08e5925ba6c7d
Tanav-Mittal <68111690+Tanav-Mittal@users.noreply.github.com>
2020-09-06 22:52:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock
+var Imgdog,ImgHappydog
+
+
+function preload()
+{
+  Imgdog = loadImage("images/dogImg.png")
+  ImgHappydog = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(500,500);
+  
+  dog = createSprite(250,250,10,10)
+  dog.addImage(Imgdog);
+  dog.scale = 0.3
+
+  foodStock = database.ref('Food')
+  foodStock.on("value",readStock)
+  
+}
+
+
+function draw() {  
+  background(46,139,87)
+
+  textSize(10);
+  fill("white")
+  stroke("blue")
+  text("NOTE: Press UP_ARROW key to Feed Drago Milk",150,50);
+  text("Food Remaining:" + foodS,200,130)
+
+
+  if(keyWentDown(UP_ARROW))
+  {
+    writeStock(foodS);
+    dog.addImage(ImgHappydog);
+  }
+
+  drawSprites();
+  //add styles here
+
+}
+
+function readStock(data)
+{
+  foodS = data.val()
+}
+
+
+function writeStock(x)
+{
+  if(x <= 0)
+  {
+    x=0;
+  }
+  else
+  {
+    x = x-1;
+  }
+
+  database.ref('/').update({
+    Food:x
+  })
+}
